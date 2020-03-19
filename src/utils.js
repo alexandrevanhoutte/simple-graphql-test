3e41a0daa435d70826974084f2b188710610dc3f
Alexandre Vanhoutte <alex@mobile-os.com>
2020-03-19 11:35:49 +0900

    first commit

---
+++ src/utils.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+const APP_SECRET = 'This-GraphQL-is-a-test'
+
+function getUserId(context) {
+  const Authorization = context.request.get('Authorization')
+  if (Authorization) {
+    const token = Authorization.replace('Bearer ', '')
+    const { userId } = jwt.verify(token, APP_SECRET)
+    return userId
+  }
+  throw new Error('Not authenticated')
+}
+
+module.exports = {
+  APP_SECRET,
+  getUserId,
+}
